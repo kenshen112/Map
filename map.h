660d7dcccef9eb3256362fe78acbf6e534770a5d
CS235 Sec 5 Group 7 <cs235s5g7@LinuxLab01.byui.edu>
2019-03-16 16:38:08 -0600

    num elements in copy constructor

--- map.h
+++ map.h
@@ -16,260 +16,261 @@
 
 namespace custom
 {
-	template <class K, class V>
-
-	class map
-	{
-
-	private:
-		BST <custom::pair<K, V>> *bst;
-
-		int numElements; // we can either use this or just call BST'S size, Tim.
-
-	public:
-		//default constructor, all constructor's should be inline in C++ Tim
-		map()
-		{
-			bst = nullptr;
-			numElements = 0;
-		}
-		//copy constructor
-		map(const map <K, V> & rhs)
-		{
-			bst = rhs.bst;
-		}
-		//assignment operator
-		map & operator=(const map <K, V> & rhs)
-		{
-			bst = rhs.bst;
-
-			return *this;
-		}
-		//destructor
-		~map()
-		{
-			bst = nullptr;
-			numElements = 0;
-			delete bst;
-		}
-
-		int size() const
-		{
-			return numElements;
-		}
-		bool empty() const
-		{
-			return numElements == 0;
-		}
-
-		void clear()
-		{
-			bst.clear();
-		}
-
-		//access functions
-		V & operator[] (const K & k) throw(const char *);
-		V operator[] (const K & k)const throw(const char *);
-
-		void insert(const K & k, const V & v) throw(const char *);
-		void insert(const pair <K, V> & input) throw(const char *);
-
-		class iterator;
-
-
-		iterator find(K k);
-		iterator begin();
-		iterator end();
-
-	};
-
-	/*****************************************************
-	 *MAP:ITERATOR
-	 *The iterator for a map
-	 *****************************************************/
-	template <class K, class V>
-	class map <K, V> ::iterator
-	{
-		//trick to this is that basically all of the methods just call
-		// the identical method in the BST iterator
-	private:
-		typename BST <custom::pair<K, V>> ::iterator bsit; // bsit, binary search iterator
-
-	public:
-		//constructors
-		iterator()
-		{}
-
-		iterator(typename BST <custom::pair<K, V>>::iterator it)
-		{
-			this->bsit = it;
-		}
-
-
-		pair<K, V>& getBst()
-		{
-			return *bsit;
-		}
-
-		iterator(const map<K, V>::iterator& it)
-		{
-			this->bsit = it.bsit;
-		}
-
-		//operators: =, ++, --, *, ==, !=
-		iterator operator = (const iterator &rhs)
-		{
-			this->bsit = rhs.bsit;
-			return *this;
-		}
-
-
-		/**************************
-		 * Increment
-		 * going up
-		 ********************/
-		iterator operator ++ ()
-		{
-			bsit++;
-			return *this;
-		}
-
-		/*********************
-		 * Decrement
-		 * going down
-		 ********************/
-		iterator operator -- ()
-		{
-			bsit--;
-			return *this;
-		}
-
-		/*****************
-		 * Dereference operator
-		 * to get to the actual data
-		 ***************/
-		pair<K, V>& operator*()
-		{
-			return *bsit;
-		}
-
-		/******************************
-		 *Equvalence operator
-		 * are two iterators pointing to the same thing
-		 *****************************/
-		bool operator == (const iterator it)
-		{
-			return this->bsit == it.bsit;
-		}
-
-		/**************************************
-		 * NOT equals operator
-		 * do two iterators point to different things
-		 *************************************/
-		bool operator != (const iterator it)
-		{
-			return this->bsit != it.bsit;
-		}
-	};
-
-	/****************************************
-	 *ITERATOR FIND
-	 *finds the key in the bst
-	 ***************************************/
-	template<class K, class V>
-	typename map<K, V>::iterator map<K, V>::find(K k)
-	{
-           pair<K, V> keyFind;
-           keyFind.first = k;
-           map<K, V>::iterator it = bst->find(keyFind);
-           
-           
-           return it;
-	}
-
-	/************************************
-	 *ITERATOR::BEGIN
-	 *returns an iterator to the beginning of the map
-	 ************************************/
-	template <class K, class V>
-	typename map<K, V>::iterator map<K, V>::begin()
-	{
-		return bst->begin();
-	}
-
-	/***************************************
-	 *ITERATOR::END
-	 *returns an iterator to the end of the map
-	 **************************************/
-	template <class K, class V>
-	typename map<K, V>::iterator map<K, V>::end()
-	{
-		return bst->end();
-	}
-	/*************************************
-	 *ACCESS OPERATOR
-	 *allows for random access
-	 ************************************/
-	template <class K, class V>
-	V & map<K, V>::operator[] (const K & k) throw(const char *)
-	{
-		pair<K, V> keyFind;
-                keyFind.first = k;
-				map<K, V>::iterator *it;
-				
-				*it = find(k);
-
-		if (it != nullptr)
-		{
-                   return it->getBst().second;
-		}
-
-		else
-		{
-			bst->insert(keyFind);
-			return find(keyFind.first).getBst().second;
-		}
-	}
-
-	/*************************************
-	 *ACCESS OPERATOR CONST
-	 *allows for random access
-	 *************************************/
-	template <class K, class V>
-	V map<K, V>::operator[] (const K & k) const throw(const char *)
-	{
-		//see above ^^
-	}
-
-	/*************************************
-	 *INSERT with K,V
-	 *inserts a new value with a key
-	 ************************************/
-	template <class K, class V>
-	void map<K, V>::insert(const K & k, const V & v) throw(const char *)
-	{
-		map::iterator *it = bst->find(k);
-
-		if (it != nullptr)
-			*it = v;
-		else
-			bst.insert(k);
-
-	}
-	/************************************
-	 *INSERT with pair <K, V>
-	 *inserts a new value using a pair
-	 ***********************************/
-	template <class K, class V>
-	void map<K, V>::insert(const pair <K, V> & input) throw(const char *)
-	{
-		map::iterator *it = bst->find(input);//please make sure I got this right
-
-		if (it != nullptr)
-			*it = input;
-		else
-			bst.insert(input);
-	}
+   template <class K, class V>
+      
+      class map
+   {
+      
+     private:
+      BST <custom::pair<K, V>> *bst;
+      
+      int numElements; // we can either use this or just call BST'S size, Tim.
+      
+     public:
+      //default constructor, all constructor's should be inline in C++ Tim
+      map()
+      {
+         bst = nullptr;
+         numElements = 0;
+      }
+      //copy constructor
+      map(const map <K, V> & rhs)
+      {
+         bst = rhs.bst;
+         numElements = rhs.numElements;
+      }
+      //assignment operator
+      map & operator=(const map <K, V> & rhs)
+         {
+            bst = rhs.bst;
+            
+            return *this;
+         }
+      //destructor
+      ~map()
+      {
+         bst = nullptr;
+         numElements = 0;
+         delete bst;
+      }
+      
+      int size() const
+      {
+         return numElements;
+      }
+      bool empty() const
+      {
+         return numElements == 0;
+      }
+      
+      void clear()
+      {
+         bst.clear();
+      }
+      
+      //access functions
+      V & operator[] (const K & k) throw(const char *);
+      V operator[] (const K & k)const throw(const char *);
+      
+      void insert(const K & k, const V & v) throw(const char *);
+      void insert(const pair <K, V> & input) throw(const char *);
+      
+      class iterator;
+      
+      
+      iterator find(K k);
+      iterator begin();
+      iterator end();
+      
+   };
+   
+   /*****************************************************
+    *MAP:ITERATOR
+    *The iterator for a map
+    *****************************************************/
+   template <class K, class V>
+      class map <K, V> ::iterator
+   {
+      //trick to this is that basically all of the methods just call
+      // the identical method in the BST iterator
+     private:
+      typename BST <custom::pair<K, V>> ::iterator bsit; // bsit, binary search iterator
+      
+     public:
+      //constructors
+      iterator()
+      {}
+      
+      iterator(typename BST <custom::pair<K, V>>::iterator it)
+      {
+         this->bsit = it;
+      }
+      
+      
+      pair<K, V>& getBst()
+      {
+         return *bsit;
+      }
+      
+      iterator(const map<K, V>::iterator& it)
+      {
+         this->bsit = it.bsit;
+      }
+      
+      //operators: =, ++, --, *, ==, !=
+      iterator operator = (const iterator &rhs)
+         {
+            this->bsit = rhs.bsit;
+            return *this;
+         }
+      
+      
+      /**************************
+       * Increment
+       * going up
+       ********************/
+      iterator operator ++ ()
+      {
+         bsit++;
+         return *this;
+      }
+      
+      /*********************
+       * Decrement
+       * going down
+       ********************/
+      iterator operator -- ()
+      {
+         bsit--;
+         return *this;
+      }
+      
+      /*****************
+       * Dereference operator
+       * to get to the actual data
+       ***************/
+      pair<K, V>& operator*()
+      {
+         return *bsit;
+      }
+      
+      /******************************
+       *Equvalence operator
+       * are two iterators pointing to the same thing
+       *****************************/
+      bool operator == (const iterator it)
+      {
+         return this->bsit == it.bsit;
+      }
+      
+      /**************************************
+       * NOT equals operator
+       * do two iterators point to different things
+       *************************************/
+      bool operator != (const iterator it)
+      {
+         return this->bsit != it.bsit;
+      }
+   };
+   
+   /****************************************
+    *ITERATOR FIND
+    *finds the key in the bst
+    ***************************************/
+   template<class K, class V>
+      typename map<K, V>::iterator map<K, V>::find(K k)
+   {
+      pair<K, V> keyFind;
+      keyFind.first = k;
+      map<K, V>::iterator it = bst->find(keyFind);
+      
+      
+      return it;
+   }
+   
+   /************************************
+    *ITERATOR::BEGIN
+    *returns an iterator to the beginning of the map
+    ************************************/
+   template <class K, class V>
+      typename map<K, V>::iterator map<K, V>::begin()
+   {
+      return bst->begin();
+   }
+   
+   /***************************************
+    *ITERATOR::END
+    *returns an iterator to the end of the map
+    **************************************/
+   template <class K, class V>
+      typename map<K, V>::iterator map<K, V>::end()
+   {
+      return bst->end();
+   }
+   /*************************************
+    *ACCESS OPERATOR
+    *allows for random access
+    ************************************/
+   template <class K, class V>
+      V & map<K, V>::operator[] (const K & k) throw(const char *)
+   {
+      pair<K, V> keyFind;
+      keyFind.first = k;
+      map<K, V>::iterator *it;
+      
+      *it = find(k);
+      
+      if (it != nullptr)
+      {
+         return it->getBst().second;
+      }
+      
+      else
+      {
+         bst->insert(keyFind);
+         return find(keyFind.first).getBst().second;
+      }
+   }
+   
+   /*************************************
+    *ACCESS OPERATOR CONST
+    *allows for random access
+    *************************************/
+   template <class K, class V>
+      V map<K, V>::operator[] (const K & k) const throw(const char *)
+   {
+      //see above ^^
+   }
+   
+   /*************************************
+    *INSERT with K,V
+    *inserts a new value with a key
+    ************************************/
+   template <class K, class V>
+      void map<K, V>::insert(const K & k, const V & v) throw(const char *)
+   {
+      map::iterator *it = bst->find(k);
+      
+      if (it != nullptr)
+         *it = v;
+      else
+         bst.insert(k);
+      
+   }
+   /************************************
+    *INSERT with pair <K, V>
+    *inserts a new value using a pair
+    ***********************************/
+   template <class K, class V>
+      void map<K, V>::insert(const pair <K, V> & input) throw(const char *)
+   {
+      map::iterator *it = bst->find(input);//please make sure I got this right
+      
+      if (it != nullptr)
+         *it = input;
+      else
+         bst.insert(input);
+   }
 }
 
 #endif // MAP_H
